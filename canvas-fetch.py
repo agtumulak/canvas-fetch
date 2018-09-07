e2cd4c02c6f8f4d633681d9303b1afbea8258e05
Aaron G. Tumulak <agtumulak@gmail.com>
2018-09-07 16:32:29 -0400

    Add requests.exceptions.ConnectionError handler

--- canvas-fetch.py
+++ canvas-fetch.py
@@ -28,35 +28,38 @@ with open(expanduser('~/.canvasfetch'), 'r') as f:
 target = expanduser(config['target'])
 headers = {'Authorization': 'Bearer {}'.format(config['token'])}
 
-with CanvasSession(base_url=config['base url'], headers=headers) as s:
-    courses = s.getjson('/api/v1/courses')
-    courses = [c for c in courses if c['course_code'] in config['courses']]
-    for course in courses:
-        logging.info("Syncing '{}'...".format(course['course_code']))
-        folders = s.getjson('/api/v1/courses/{}/folders'.format(course['id']))
-        folders = {folder['id']: folder for folder in folders}
-        files = s.getjson('/api/v1/courses/{}/files'.format(course['id']))
-        for file in files:
-            folder = folders[file['folder_id']]
-            path = join(
-                    target,
-                    course['course_code'],
-                    folder['full_name'],
-                    file['filename'])
-            remote_modified = dateutil.parser.parse(file['modified_at'])
-            if exists(path):
-                local_modified = (
-                        datetime
-                        .fromtimestamp(getmtime(path))
-                        .astimezone(remote_modified.tzinfo))
-                if local_modified == remote_modified:
-                    continue
-            # If this is reached, the file needs to be downloaded
-            logging.info("Downloading file '{}'...".format(path))
-            if not exists(dirname(path)):
-                makedirs(dirname(path))
-            with open(path, 'wb') as f:
-                f.write(s.get(file['url']).content)
-            utime(
-                    path,
-                    (remote_modified.timestamp(), remote_modified.timestamp()))
+try:
+    with CanvasSession(base_url=config['base url'], headers=headers) as s:
+        courses = s.getjson('/api/v1/courses')
+        courses = [c for c in courses if c['course_code'] in config['courses']]
+        for course in courses:
+            logging.info("Syncing '{}'...".format(course['course_code']))
+            folders = s.getjson('/api/v1/courses/{}/folders'.format(course['id']))
+            folders = {folder['id']: folder for folder in folders}
+            files = s.getjson('/api/v1/courses/{}/files'.format(course['id']))
+            for file in files:
+                folder = folders[file['folder_id']]
+                path = join(
+                        target,
+                        course['course_code'],
+                        folder['full_name'],
+                        file['filename'])
+                remote_modified = dateutil.parser.parse(file['modified_at'])
+                if exists(path):
+                    local_modified = (
+                            datetime
+                            .fromtimestamp(getmtime(path))
+                            .astimezone(remote_modified.tzinfo))
+                    if local_modified == remote_modified:
+                        continue
+                # If this is reached, the file needs to be downloaded
+                logging.info("Downloading file '{}'...".format(path))
+                if not exists(dirname(path)):
+                    makedirs(dirname(path))
+                with open(path, 'wb') as f:
+                    f.write(s.get(file['url']).content)
+                utime(
+                        path,
+                        (remote_modified.timestamp(), remote_modified.timestamp()))
+except requests.exceptions.ConnectionError as e:
+    logging.warn('Connection error. Make sure you are connected to the Innernette.')
